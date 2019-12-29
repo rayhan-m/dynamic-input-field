b97b2efa5e915c31df2d8ab86767746799534d78
rayhan-m <1000157@daffodil.ac>
2019-12-30 03:06:27 +0600

    first commit

---
+++ app/Http/Controllers/DynamicFieldController.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Validator;
+use App\DynamicField;
+
+class DynamicFieldController extends Controller
+{
+    function index()
+    {
+     return view('dynamic_field');
+    }
+
+    function insert(Request $request)
+    {
+    	try {
+    		//return $request;
+    	//dd($request);
+     if($request->ajax())
+     {
+      $rules = array(
+       'first_name.*'  => 'required',
+       'last_name.*'  => 'required'
+      );
+      $error = Validator::make($request->all(), $rules);
+      if($error->fails())
+      {
+       return response()->json([
+        'error'  => $error->errors()->all()
+       ]);
+      }
+
+      $first_name = $request->first_name;
+      $last_name = $request->last_name;
+      for($count = 0; $count < count($first_name); $count++)
+      {
+       $data = array(
+        'first_name' => $first_name[$count],
+        'last_name'  => $last_name[$count]
+       );
+       $insert_data[] = $data; 
+      }
+
+      DynamicField::insert($insert_data);
+      return response()->json([
+       'success'  => 'Data Added successfully.'
+      ]);
+     }
+    	} catch (Exception $e) {
+    		
+    	}
+    }
+}
